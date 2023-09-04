71e4a7b0c32f31a4380becbf284afb9998ada573
pavantej979 <134992225+pavantej979@users.noreply.github.com>
2023-09-04 11:16:39 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes
+{
+   static int i;
+   static float f;
+   static char ch;
+   static double d;
+   static short s;
+   static long l;
+   static byte b;
+   static boolean bl;
+   public static void main (String args[])
+      {
+           System.out.println("Defalut value of int type= "+i);
+           System.out.println("Defalut value of float type= "+f);
+           System.out.println("Defalut value of char type= "+ch);
+           System.out.println("Defalut value of double type= "+d);
+           System.out.println("Defalut value of float type= "+s);
+           System.out.println("Defalut value of float type= "+l);
+           System.out.println("Defalut value of float type= "+b);
+           System.out.println("Defalut value of float type= "+bl);
+      }
+}
